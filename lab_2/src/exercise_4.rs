c3894096de62ee31348b277e7cb39e659b2762d2
emmanuelrobert7 <robertemmanuel393@gmail.com>
2026-07-30 20:50:31 +0100

    rust labs with README and stretch goals added

--- lab_2/src/exercise_4.rs
+++ lab_2/src/exercise_4.rs
@@ -1,17 +1,16 @@
-// Lab Session 2 - Control Flow & Pattern Matching
-// Exercise 2.5: FizzBuzz with match
-
+//exercise 2.5
 fn fizzbuzz(n: u32) -> String {
     match (n % 3, n % 5) {
         (0, 0) => String::from("FizzBuzz"),
         (0, _) => String::from("Fizz"),
-        // TODO 5: Complete the remaining arms
         (_, 0) => String::from("Buzz"),
+
+        // TODO 5
         _ => n.to_string(),
     }
 }
 
-fn main() {
+pub fn main() {
     for i in 1..=50 {
         println!("{}", fizzbuzz(i));
     }
